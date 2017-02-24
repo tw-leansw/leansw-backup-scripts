bc1620c1be59d07981574aed86236290e3b30ab8
qmxie <qmxie@thoughtworks.com>
2017-02-24 14:54:34 +0800

    [#0] modify gocd backup script

--- gocd-backup.sh
+++ gocd-backup.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 # Requirement: jq (eg. yum install -y jq)
-# RESPONSE=curl 'http://GOCD_DOMAIN:8153/go/api/backups' \
-#       -u 'GOCD_USER:GOCD_PWD' \
-#       -H 'Confirm: true' \
-#       -H 'Accept: application/vnd.go.cd.v1+json' \
-#       -X POST
-RESPONSE=$(cat /root/backup/response.json)
+
+RESPONSE=$(curl "http://${GOCD_DOMAIN_NAME:=gocd.dev.twleansw.com}:8153/go/api/backups" \
+      -u '${GOCD_USER}:${GOCD_PASSWORD}' \
+      -H 'Confirm: true' \
+      -H 'Accept: application/vnd.go.cd.v1+json' \
+      -X POST)
 
 BACKUP_PATH=$(echo $RESPONSE|jq -r '.path' )
 BACKUP_DIR=$(echo $RESPONSE |jq -r '.path|split("/")|last')
 echo "BACKUP_PATH:" $BACKUP_PATH
 echo "BACKUP_DIR:" $BACKUP_DIR
-docker cp gocd-server:/$BACKUP_PATH /tmp/
-scp -r /tmp/$BACKUP_DIR root@backup.demo.twleansw.com:/root/backup/deliflow-gocd/
-#docker exec gocd-server rm -rf
+
+GOCD_HOST=${GOCD_HOST:=gocd.dev.twleansw.com}
+GOCD_CONTAINER_NAME=${GOCD_CONTAINER_NAME:=gocd-server}
+
+ssh root@$GOCD_HOST << EOF
+docker cp $GOCD_CONTAINER_NAME:$BACKUP_PATH /tmp/
+cd /tmp
+tar -czf $BACKUP_DIR.tgz $BACKUP_DIR
+rm -rf /tmp/$BACKUP_DIR
+docker exec $GOCD_CONTAINER_NAME rm -rf $BACKUP_PATH
+EOF
+
+scp -r root@$GOCD_HOST:/tmp/$BACKUP_DIR.tgz /root/backup/deliflow-gocd/
+find /root/backup/deliflow-gocd/ -type f -mtime +10 -name '*.tgz' | xargs rm -rf
